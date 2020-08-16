d3ab5c744bf17047c15f7c287cd1671739087376
shvashakidze <shvashakidze@users.noreply.github.com>
2020-08-16 04:30:27 -0700

    Add dotenv package

--- myApp.js
+++ myApp.js
@@ -31,8 +31,8 @@ app.use('/public', express.static(__dirname + "/public"));
 
 /** 6) Use the .env file to configure the app */
  app.get("/json", function(req, res){
-    console.log(process.env.MESSAGE_STYLE, ' <= message style'); 
-    if (process.env.MASSAGE_STYLE === "uppercase"){
+    console.log(process.env.MESSAGE_STYLE, " <= message style"); 
+    if (process.env.MESSAGE_STYLE === "uppercase"){
          res.json(
              {"mesage": "HELLO JSON"}
              )
